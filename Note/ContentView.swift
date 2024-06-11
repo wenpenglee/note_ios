220c4cc7a64b46e48926154a9f0b9117149a234f
Wen Peng Lee <leewenpengl@gmail.com>
2024-06-11 11:10:11 +0800

    Initial Commit

---
+++ Note/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Note
+//
+//  Created by Digiever on 2024/6/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Note/NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteApp.swift
+//  Note
+//
+//  Created by Digiever on 2024/6/11.
+//
+
+import SwiftUI
+
+@main
+struct NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
